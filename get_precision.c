62d8c9a7321068acf110eeac3e16393678a264f0
mofisam <malaosamuel2020@gmail.com>
2023-09-18 18:08:42 -0700

    Group get_precision.c file by V $ S

--- get_precision.c
+++ get_precision.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * get_precision - function that calculates the
- * precision for printing
+ * get_precision - function that calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
+ *
  * Return: Precision is returned.
  */
 int get_precision(const char *format, int *i, va_list list)
@@ -39,4 +39,3 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
-
