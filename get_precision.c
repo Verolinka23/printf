165c48fda6a6bbea916ff4e7d6fb9ad32f9a92b5
Verolinka23 <osasumwenveronica@gmail.com>
2023-09-19 15:09:40 -0700

    Group get_precision file by V and S

--- get_precision.c
+++ get_precision.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include "HEADER FILE"
 
 /**
- * get_precision - function calculates the precision for printing
+ * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
